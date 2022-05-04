151fbc39446b50a1928bde64731d9f0235c43f69
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-05-04 09:55:44 +0530

    Add files via upload

---
+++ catalog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+# username:admin,password:adminpass
+# below done while registering models-step befor views and after models
+from .models import Author,Genre,Language,Book,BookInstance
+# Register your models here.
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Book)
+admin.site.register(BookInstance)
---
+++ views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book,BookInstance,Genre,Language
+from django.views.generic import CreateView,DetailView,ListView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+# Create your views here.
+def index(request):
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    num_instances_avail=BookInstance.objects.filter(status__exact='a').count()
+    context={
+      'num_books':num_books,
+      'num_instances':num_instances,
+      'num_instances_avail':num_instances_avail,
+    }
+    return render(request,'catalog/index.html',context=context)
+class BookCreate(CreateView):#model_form.html
+    model=Book
+    fields='__all__'
+
+    # success_url=
+class BookDetail(DetailView):
+    model=Book
+@login_required
+def my_view(request):
+    return render(request,'catalog/my_view.html')
+
+class SignUpView(CreateView)   :
+    form_class=UserCreationForm
+    success_url=reverse_lazy('login')
+    template_name='catalog/signup.html'
+class CheckedOutBooksByUserView(LoginRequiredMixin,ListView):
+    #List all BookInstancesBut filter based off currently
+    #logged in user session
+    model=BookInstance
+    template_name='catalog/profile.html'
+    paginate_by= 5 #5book instances per page
+
+    def get_queryset(self) :
+        return BookInstance.objects.filter(borrower=self.request.user)
